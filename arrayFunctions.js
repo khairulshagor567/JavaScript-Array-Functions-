f1ec4817e5fdfecb38aff9c4d2a6d6bc21e76a05
khairul islam <khairulsagor567@gmail.com>
2026-02-12 14:43:32 +0600

    Every array function's problems are completed

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -117,3 +117,18 @@ const allBlueEyes = characters.every((character)=>character.eye_color =='blue');
 //2.Does every character have mass more than 40?
 const allMass = characters.every((character)=>character.mass >40);
 //console.log(allMass);
+
+//3.Is every character shorter than 200?
+const allShorterThan200 = characters.every((character)=>character.height < 200);
+//console.log(allShorterThan200);
+
+//4.Is every character male?
+const allMale = characters.every((character)=>character.gender == 'male');
+//console.log(allMale);
+
+
+
+
+
+
+
